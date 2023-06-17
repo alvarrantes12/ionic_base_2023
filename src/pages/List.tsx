3b74f3e446135f898710744d5849971d55cec3ec
IsaacVG11 <isaac.villalobosg@gmail.com>
2023-06-17 11:37:52 -0600

    IVG-C18512 Ionic

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,58 @@
 
-import { useParams } from 'react-router';;
 import './List.css';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonButton,
+} from '@ionic/react';
+
+import ApiMethods from '../commons/ApiMethods';
 
 const List: React.FC = () => {
 
-  return (
-    <div>Hola</div>
-  );
+  const { data , refetch } = ApiMethods('http://localhost:3000/pets');
+
+  if (!data) {
+    return <h1> Cargando...</h1>
+  } else {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>
+              Ejemplo Ionic #1
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          { data?.map((pet: any) => {
+            return (
+              <IonCard className='ion-card'>
+                <IonCardHeader>
+                  <IonCardTitle className='ion-card-title'>Nombre: {pet.name}</IonCardTitle>
+                  <IonCardSubtitle className='ion-card-subtitle'>Raza: {pet.breed}</IonCardSubtitle>
+                </IonCardHeader>
+                <IonCardContent className='ion-card-content'>Dueñ@: {pet.owner.first_name}</IonCardContent>
+              </IonCard>
+            )
+          })}
+
+          <IonButton className='ion-button' expand='block' onClick={refetch}>
+            Agregar
+          </IonButton>
+        </IonContent>
+      </IonPage>
+    )
+  }
+
 };
 
 export default List;
