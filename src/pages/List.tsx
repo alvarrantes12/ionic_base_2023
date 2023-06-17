24feb89cf6ce80de33994197e15fbf581aedd735
Anderson1616 <ANDERSON.UMANA@ucr.ac.cr>
2023-06-17 10:57:15 -0600

    Anderson-C19438 IONIC

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,59 @@
 
-import { useParams } from 'react-router';;
 import './List.css';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonButton
+} from "@ionic/react";
+
+import ApiMethods from '../commons/AphiMethods';
 
 const List: React.FC = () => {
 
-  return (
-    <div>Hola</div>
-  );
+  const {data, refetch} = ApiMethods('http://localhost:3000/pets');
+
+  if (!data) {
+    return <h1>Cargando...</h1>
+  } else {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>
+              Ejemplo Ionic #1
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          {data?.map((pet: any) => {
+            return (
+              <IonCard className='Ion__Card'>
+                <IonCardHeader>
+                  <IonCardTitle className='Ion__Card__Title'>Nombre: {pet.name}</IonCardTitle>
+                  <IonCardSubtitle className='Ion__Card__SubTitle'>Raza: {pet.breed}</IonCardSubtitle>
+                </IonCardHeader>
+
+                <IonCardContent className='Ion__Card__Content'>Mi dueño es: {pet.owner.first_name}</IonCardContent>
+              </IonCard>
+            )
+          })}
+
+          <IonButton className='Ion_Button' expand='block' color='danger' onClick={refetch}>
+            Agregar
+          </IonButton>
+        </IonContent>
+      </IonPage>
+    )
+  }
+
 };
 
 export default List;
