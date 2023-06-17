846ee1a30b11351cdad94d83eeb8a19f4fc3df97
AlexaVegaRojas <alexavr92@gmail.com>
2023-06-17 11:28:05 -0600

    avr-C09317 part one ionic

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,53 @@
-
-import { useParams } from 'react-router';;
 import './List.css';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonButton
+} from "@ionic/react";
+import ApiMethods from '../commons/ApiMethods';
 
 const List: React.FC = () => {
+  const { data, refetch } = ApiMethods('http://localhost:3000/pets');
+  if (!data){
+    return <h1> Cargando información</h1>
+  } else {
+    return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            Ejemplo Ionic Uno
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        {data?.map((pet: any) => {
+            return(
+            <IonCard className= 'Ion__Card'>
+              <IonCardHeader>
+                  <IonCardTitle className= 'Ion__Card__Title'>Nombre {pet.name} </IonCardTitle>
+                  <IonCardSubtitle className= 'Ion__Card__SubTitle'>Raza {pet.breed}</IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent className= 'Ion__Card__Content'>Dueño de la mascota {pet.owner.first_name}</IonCardContent>
+            </IonCard>
+            )
+        })}
+        <IonButton className = 'Ion_Button' expand="block" color= "danger" onClick= {refetch}>
+          Agregar
+        </IonButton>
+      </IonContent>
+    </IonPage>
+    )
+  }
 
-  return (
-    <div>Hola</div>
-  );
 };
 
 export default List;
