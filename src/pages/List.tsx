c1586efad82092344c5d73d8eac488d458bb9f97
tyrsergio <sergio.hernandezjimenez@ucr.ac.cr>
2023-06-17 10:58:31 -0600

    SHJ-C13656 IonicDOne

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,66 @@
 
-import { useParams } from 'react-router';;
 import './List.css';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle, 
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonSpinner,
+  IonButton
+} from "@ionic/react";
+import ApiMethods from '../commons/ApiMethods';
+
 
 const List: React.FC = () => {
 
-  return (
-    <div>Hola</div>
-  );
+  const {data, refetch} = ApiMethods('http://localhost:3000/pets')
+
+  if(!data){
+    
+    return <h1>
+      <IonItem>
+        <IonLabel>Cargando</IonLabel>
+        <IonSpinner name="circles"></IonSpinner>
+      </IonItem>
+    </h1>
+
+  }else{
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>
+              Ejemplo Ionic #1
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          {data?.map((pet: any) => {
+            return (
+              <IonCard className='Ion__Card'>
+                <IonCardHeader>
+                  <IonCardTitle className='Ion__Card__Title'>Nombre: {pet.name}</IonCardTitle>
+                  <IonCardSubtitle className='Ion__Card__Subtitle'>Raza: {pet.breed}</IonCardSubtitle>
+                </IonCardHeader>
+                  <IonCardContent className='Ion__Card__Content'>Mi dueño es: {pet.owner.first_name}</IonCardContent>
+              </IonCard>
+            )
+          })}
+          <IonButton expand="block" color="danger" onClick={refetch}>
+            Agregar
+          </IonButton>
+        </IonContent>
+      </IonPage>
+    )
+  }
 };
 
 export default List;
