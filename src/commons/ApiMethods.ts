3328eddc6d4f4dcd89d99d7537fc83671a0447dd
davidrorez <davidrorez@gmail.com>
2023-06-19 18:29:14 -0600

    Ionic Example done

--- src/commons/ApiMethods.ts
+++ src/commons/ApiMethods.ts
@@ -35,9 +35,23 @@ function ApiMethods(url: any) {
             .then((response) => { setData(response.data) })
             .catch((err) => { setError(err) })
             .finally(() => { setLoading(false) })  
-    }   
+    } 
+    
+    const putMethod = (id: any, name: any) => {
+        const config = {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        }
+        setLoading(true);
+        axios.put(`${url}/${id}`, {name: name}, config)
+            .then((response) => { setData(response.data) })
+            .catch((err) => { setError(err) })
+            .finally(() => { setLoading(false) })
+    }
 
-    return { data, loading, error, refetch }
+    return { data, loading, error, refetch, putMethod }
 }
 
 export default ApiMethods;
