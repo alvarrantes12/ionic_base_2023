94c6939b5638e71cf5227d981e138392397c231d
JoseIsaacLazo <jose.lazo@ucrso.info>
2023-06-19 18:29:52 -0600

    Created a form with editing functionality and visual aspects

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -15,9 +15,11 @@ import {
 } from "@ionic/react";
 import ApiMethods from '../commons/ApiMethods';
 
+import { environment } from '../environments/environment.dev';
+
 const List: React.FC = () => {
 
-  const {data, refetch} = ApiMethods('http://localhost:3000/pets');
+  const {data, refetch} = ApiMethods(`${environment.apiEndpoint}/pets`);
 
   if (!data) {
     return <h1>Estamos cargando su información...</h1>
